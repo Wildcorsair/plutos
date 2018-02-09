e9763d69ce92c09fe07769a23699ee2d88f3190b
test <test@gmail.com>
2018-02-09 14:51:37 +0200

    Initial commit

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 2/9/18
+ * Time: 2:24 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexAction()
+    {
+        return new Response('Home page!');
+    }
+}
