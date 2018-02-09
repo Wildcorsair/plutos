25a646088d5bbbd3c0363584e35fcd5537696202
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-02-09 15:01:18 +0200

    Added controllers and routes for Home and About pages

---
+++ src/AppBundle/Controller/AboutController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 2/9/18
+ * Time: 2:53 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AboutController extends Controller
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function indexAction()
+    {
+        return new Response('About Page');
+    }
+}
